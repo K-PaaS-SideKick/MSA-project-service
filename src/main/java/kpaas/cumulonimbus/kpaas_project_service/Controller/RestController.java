5cede0024bc9a4447ad48716b3df85aba37316f9
SangWookie <swcube520@gmail.com>
2024-08-01 17:41:00 +0900

    add: '/' for liveness probe

--- src/main/java/kpaas/cumulonimbus/kpaas_project_service/Controller/RestController.java
+++ src/main/java/kpaas/cumulonimbus/kpaas_project_service/Controller/RestController.java
@@ -1,9 +1,16 @@
 package kpaas.cumulonimbus.kpaas_project_service.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+    @GetMapping("/")
+    public ResponseEntity<?> home() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
