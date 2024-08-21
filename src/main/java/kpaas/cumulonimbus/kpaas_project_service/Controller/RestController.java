4300cb5c2e079f73d3e43c3eb47fb200f09cc88f
SangWookie <swcube520@gmail.com>
2024-08-21 21:20:45 +0900

    vault settings done

--- src/main/java/kpaas/cumulonimbus/kpaas_project_service/Controller/RestController.java
+++ src/main/java/kpaas/cumulonimbus/kpaas_project_service/Controller/RestController.java
@@ -3,10 +3,12 @@ package kpaas.cumulonimbus.kpaas_project_service.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+//@RequestMapping("/project")
 public class RestController {
-    @GetMapping("/")
+    @GetMapping("/status")
     public ResponseEntity<?> home() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
