7bd50b9cc75726ea342528e0fcbdc8965ca58bf0
SangWookie <swcube520@gmail.com>
2024-10-30 16:45:29 +0900

    fix: project summary

--- src/main/java/kpaas/cumulonimbus/kpaas_project_service/DTO/ProjectSummary.java
+++ src/main/java/kpaas/cumulonimbus/kpaas_project_service/DTO/ProjectSummary.java
@@ -1,11 +1,14 @@
 package kpaas.cumulonimbus.kpaas_project_service.DTO;
 
+import kpaas.cumulonimbus.kpaas_project_service.Entity.Image;
 import kpaas.cumulonimbus.kpaas_project_service.Entity.Project;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,6 +26,7 @@ public class ProjectSummary {
     private String status;
     private int max_members;
     private int current_members;
+    private List<Long> images = new ArrayList<>();
 
     public ProjectSummary(Project project){
         this.pid = project.getPid();
@@ -37,5 +41,8 @@ public class ProjectSummary {
         this.status = project.getStatus();
         this.max_members = project.getMax_members();
         this.current_members = project.getCurrent_members();
+        for(Image image : project.getImages()){
+            this.images.add(image.getId());
+        }
     }
 }
